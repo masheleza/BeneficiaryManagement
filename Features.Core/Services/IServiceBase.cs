f8bada6e27df1cabf3eca16ea282e383921fcc7c
Zandile Mashele <masehleza@gmail.com>
2021-05-14 09:53:44 +0200

    Added service base class

--- Features.Core/Services/IServiceBase.cs
+++ Features.Core/Services/IServiceBase.cs
@@ -1,7 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
 namespace Features.Core.Services
 {
-    public interface IServiceBase
+    public interface IServiceBase<T>
     {
+        Task<T> Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        Task<List<T>> Get();
     }
 }
---
+++ Features.Core/Services/ServiceBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Features.Core.Services
+{
+    public class ServiceBase<T>: IServiceBase<T>
+    {
+        public ServiceBase()
+        {
+        }
+
+        public Task<T> Create(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

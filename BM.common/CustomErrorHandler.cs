00c8ed0828f2f53ed4e963718166cc8377411f7e
Zandile Mashele <masehleza@gmail.com>
2021-05-14 09:52:56 +0200

    * CustomErrorHandler.cs: added error handler
    * ApiResult.cs: Added API Result

---
+++ BM.common/CustomErrorHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace BM.common
+{
+    public class CustomErrorHandler
+    {
+        public string ErrorMessage { get; set; }
+        public string PropertyName { get; set; }
+
+        public CustomErrorHandler(string errorMessage)
+        {
+            this.ErrorMessage = errorMessage;
+            this.PropertyName = string.Empty;
+        }
+
+        public CustomErrorHandler(string errorMessage, string prop)
+        {
+            this.ErrorMessage = errorMessage;
+            this.PropertyName = prop;
+        }
+
+        public override string ToString()
+        {
+            return $"Property {PropertyName}, error message {ErrorMessage}";
+        }
+    }
+}
---
+++ Features.Core/Helpers/ApiResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using BM.common;
+
+namespace Features.Core.Helpers
+{
+    public class ApiResult<T>
+    {
+        public T Data { get; set; }
+        public bool Success { get; set; }
+        public List<CustomErrorHandler> ErrorList { get; set; }
+
+        public ApiResult()
+        {
+            ErrorList = new List<CustomErrorHandler>();
+        }
+    }
+}
